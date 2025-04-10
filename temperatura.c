291ff91e3238f697afdb791ba511032bf8d995af
Santiago <santiago.palaciova@upb.edu.co>
2025-04-10 09:38:49 -0500

    se añade trabajo de clase

--- temperatura.c
+++ temperatura.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <time.h>
-#include <stlib.h>
+#include <stdlib.h>
+#include <math.h>
 int main(){
 
-srad(time(NULL));
-
+srand(time(NULL)); // generamo0s una semilla 
 
      int temp[24]; /* Con esto ya tenemos declaradas las 24 variables */
      float media = 0;
@@ -12,10 +12,42 @@ srad(time(NULL));
      /* Ahora tenemos que dar el valor de cada una */
      for( hora=0; hora<24; hora++ )
           {
-          rand();
+          temp[hora] = (rand() % ((35-14)+1))+14;
           media += temp[hora];
+          printf("%d\n", temp[hora]);
           }
      media = media / 24;
-     printf("\nLa temperatura media es %f\n", media );
+     printf("\nLa temperatura media es %f \n", media );
 
-     return 0,}
+     float mayor = temp[0];
+     float menor = temp[0];
+          for(int i =0; i<24; i++)
+         {
+
+          if(temp[i] > mayor)
+          {mayor = temp[i];}
+
+          else
+          {menor = temp[i];}
+
+
+
+          
+
+          }
+
+          printf("El mayor es %f y el menor es %f\n", mayor, menor);
+
+          float promedio_limites = (mayor + menor)/2;
+          printf("el promedi por limites es %f\n", promedio_limites);
+
+          float dif_promedios = media - promedio_limites;
+
+          printf("la diferencia de promedios es %f\n", abs(dif_promedios));
+
+
+     
+
+     return 0;}
+
+     // {}
